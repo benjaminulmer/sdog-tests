736f4382705380cd423ff4f8f9ac2942e0b9bfe4
blulmer <benjaminulmer55@gmail.com>
2018-08-21 11:09:32 -0600

    Added copy of SDOG code from subdivision-framework

---
+++ sdog-tests/SdogCell.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include <string>
+#include <vector>
+
+// Enum for the three different SDOG cell types plus an invalid cell code flag
+enum class SdogCellType {
+	NG,
+	LG,
+	SG,
+	INVALID
+};
+
+// Class for storing information about an SDOG cell and performing queries
+class SdogCell {
+
+public:
+	SdogCell(const std::string& code, double gridRadius);
+	SdogCell(SdogCellType type, double minLat, double maxLat, double minLong, double maxLong, double minRad, double maxRad);
+
+	bool contains(double latRad, double longRad, double radius);
+
+	void children(std::vector<std::string>& out);
+	void neighbours(std::vector<std::string>& out);
+
+	double volume();
+	double surfaceArea();
+	double sphericity();
+
+	static std::string codeForPos(double latRad, double longRad, double radius, double gridRadius, unsigned int level);
+	static bool codeIsValid(std::string code);
+
+	std::string getCode() const { return code; }
+	double getMinLat() const { return minLat; }
+	double getMaxLat() const { return maxLat; }
+	double getMinLong() const { return minLong; }
+	double getMaxLong() const { return maxLong; }
+	double getMinRad() const { return minRad; }
+	double getMaxRad() const { return maxRad; }
+	double getGridRad() const { return gridRadius; }
+	SdogCellType getType() const { return type; }
+
+private:
+	std::string code;
+
+	double minLat, maxLat;
+	double minLong, maxLong;
+	double minRad, maxRad;
+	double gridRadius;
+
+	SdogCellType type;
+};
