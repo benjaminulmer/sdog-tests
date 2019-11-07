d6df97365105ec143d0c84dadb5231b79aa32b41
blulmer <benjaminulmer55@gmail.com>
2019-11-07 14:25:43 -0700

    Added tests for sphericity min, max, and SD

--- sdog-tests/Program.cpp
+++ sdog-tests/Program.cpp
@@ -8,25 +8,27 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <limits>
 #include <time.h>
 
 void Program::start() {
 
 	clock_t t = clock();
 
-	const int maxSL = 15;
+	constexpr int MAX_SL = 15;
 
-	unsigned long long numCells[maxSL + 1]; for (int i = 0; i <= maxSL; i++) numCells[i] = SdogCell::numCells(i);
+	unsigned long long numCells[MAX_SL + 1]; for (int i = 0; i <= MAX_SL; i++) numCells[i] = SdogCell::numCells(i);
 
-	double min[maxSL + 1]; for (int i = 0; i <= maxSL; i++) min[i] = 10000.0;
-	double max[maxSL + 1]; for (int i = 0; i <= maxSL; i++) max[i] = -1.0;
-	double meanVol[maxSL + 1]; for (int i = 0; i <= maxSL; i++) meanVol[i] = M_PI / (6.0 * numCells[i]);
-	double meanSA[maxSL + 1]; for (int i = 0; i <= maxSL; i++) meanSA[i] = 0.0;
-	double meanSph[maxSL + 1]; for (int i = 0; i <= maxSL; i++) meanSph[i] = 0.0;
+	double minVol[MAX_SL + 1]; for (int i = 0; i <= MAX_SL; i++) minVol[i] = std::numeric_limits<double>::max();
+	double maxVol[MAX_SL + 1]; for (int i = 0; i <= MAX_SL; i++) maxVol[i] = -1.0;
+	double meanVol[MAX_SL + 1]; for (int i = 0; i <= MAX_SL; i++) meanVol[i] = M_PI / (6.0 * numCells[i]);
 
-	double SDVol[maxSL + 1]; for (int i = 0; i <= maxSL; i++) SDVol[i] = 0.0;
-	double SDSA[maxSL + 1]; for (int i = 0; i <= maxSL; i++) SDSA[i] = 0.0;
-	double SDSph[maxSL + 1]; for (int i = 0; i <= maxSL; i++) SDSph[i] = 0.0;
+	double minSph[MAX_SL + 1]; for (int i = 0; i <= MAX_SL; i++) minSph[i] = std::numeric_limits<double>::max();
+	double maxSph[MAX_SL + 1]; for (int i = 0; i <= MAX_SL; i++) maxSph[i] = -1.0;
+	double meanSph[MAX_SL + 1]; for (int i = 0; i <= MAX_SL; i++) meanSph[i] = 0.0;
+
+	double SDVol[MAX_SL + 1]; for (int i = 0; i <= MAX_SL; i++) SDVol[i] = 0.0;
+	double SDSph[MAX_SL + 1]; for (int i = 0; i <= MAX_SL; i++) SDSph[i] = 0.0;
 
 	// Functions
 	auto defFunc = [&](double max, double min, SdogCellType type) {
@@ -42,8 +44,8 @@ void Program::start() {
 		}
 	};
 
-	double a = 0.539893087;
-	//double a = 0.57;
+	//double a = 0.539893087;
+	double a = 0.57;
 	auto latConstFunc = [&](double max, double min, SdogCellType type) {
 		return (type == SdogCellType::SG) ? a * max + (1 - a) * min : 0.5 * max + 0.5 * min;
 	};
@@ -66,11 +68,17 @@ void Program::start() {
 	};
 	// End functions
 
-	std::ofstream out("test3.txt");
+	// Open file for output
+	std::ofstream out("b05.csv");
 	if (!out.is_open()) {
 		return;
 	}
 
+	// Set funcs to use for subdivision
+	auto latSub = latBalFunc;
+	auto radSub = radBalFunc;
+
+	// Pass #1
 	std::vector<SdogCell> queue;
 	queue.push_back(SdogCell(0, SdogCellType::SG, 0.0, M_PI_2, 0.0, M_PI_2, 0.0, 1.0));
 	unsigned long long count = 0;
@@ -86,31 +94,67 @@ void Program::start() {
 		int SL = c.getSL();
 		int n = c.numSimilarInOct();
 
-		meanSA[SL] += sa * n;
+		minSph[SL] = (sph < minSph[SL]) ? sph : minSph[SL];
+		maxSph[SL] = (sph > maxSph[SL]) ? sph : maxSph[SL];
 		meanSph[SL] += sph * n;
+
+		minVol[SL] = (v < minVol[SL]) ? v : minVol[SL];
+		maxVol[SL] = (v > maxVol[SL]) ? v : maxVol[SL];
 		SDVol[SL] += (v - meanVol[SL]) * (v - meanVol[SL]) * n;
 
-		min[SL] = (v < min[SL]) ? v : min[SL];
-		max[SL] = (v > max[SL]) ? v : max[SL];
-
-		if (c.getSL() < maxSL) {
-			c.sliceSubdivide(queue, latVolFunc, radVolFunc);
+		// Subdivision type here
+		if (c.getSL() < MAX_SL) {
+			c.sliceSubdivide(queue, latSub, radSub);
 		}
 		count++;
 
 		if (count % 100000000 == 0) {
-			std::cout << count << " : ~" << numCells[maxSL] << std::endl;
+			std::cout << count << " : ~" << numCells[MAX_SL] << std::endl;
 		}
 	}
 
-	for (int i = 1; i <= maxSL; i++) {
-
-		meanSA[i] /= numCells[i];
+	for (int i = 1; i <= MAX_SL; i++) {
 		meanSph[i] /= numCells[i];
+	}
+
+	// Pass #2
+	queue.push_back(SdogCell(0, SdogCellType::SG, 0.0, M_PI_2, 0.0, M_PI_2, 0.0, 1.0));
+	count = 0;
+	while (queue.size() > 0) {
+
+		SdogCell c = queue.back();
+		queue.pop_back();
+
+		double v = c.volume();
+		double sa = c.surfaceArea();
+		double sph = (pow(M_PI, 1.0 / 3.0) * pow(6.0 * v, 2.0 / 3.0)) / sa;
+
+		int SL = c.getSL();
+		int n = c.numSimilarInOct();
+
+		SDSph[SL] += (sph - meanSph[SL]) * (sph - meanSph[SL]) * n;
+
+		// Subdivision type here
+		if (c.getSL() < MAX_SL) {
+			c.sliceSubdivide(queue, latSub, radSub);
+		}
+		count++;
+
+		if (count % 100000000 == 0) {
+			std::cout << count << " : ~" << numCells[MAX_SL] << std::endl;
+		}
+	}
+
+	// Report
+	out << "k,maxVol,minVol,meanVol,SDVol,maxSph,minSph,meanSph,SDSph" << std::endl;
+
+	for (int i = 1; i <= MAX_SL; i++) {
+
 		SDVol[i] = sqrt(SDVol[i] / numCells[i]);
+		SDSph[i] = sqrt(SDSph[i] / numCells[i]);
 		out << std::setprecision(17);
-		//out << i << "::: " << max[i] / min[i] << std::endl;
-		out << i << "," << max[i] << "," << min[i] << "," << meanVol[i] << "," << meanSA[i] << "," << meanSph[i] << "," << SDVol[i] << std::endl;
-		//out << i << "," << max[i] << "," << min[i] << "," << meanVol[i] << "," << meanSA[i] << "," << meanSph[i] << "," << SDVol[i] << std::endl;
+		
+		out << i << "," << maxVol[i] << "," << minVol[i] << "," << meanVol[i] << "," << SDVol[i];
+		out << "," << maxSph[i] << "," << minSph[i] << "," << meanSph[i] << "," << SDSph[i] << std::endl;
 	}
 }
